a865378eae45b59718ac00f558662df5e2b3dca2
DrGFreeman <DrGFreeman@users.noreply.github.com>
2017-10-14 15:25:13 -0400

    Added GUI

---
+++ playgui.py
@@ -0,0 +1,177 @@
+import pickle
+import random
+import sys
+import time
+
+import pygame as pg
+import pygame.locals
+
+import numpy as np
+import cv2
+
+import rpsutil as rps
+import rpsimgproc as imp
+import rpsgui
+
+CAPTION = 'Rock-Paper-Scissors by drgfreeman@tuta.io'
+
+def saveImage(img, gesture, notify=False):
+
+    # Define image path and filename
+    folder = rps.imgPathsRaw[gesture]
+    name = rps.gestureTxt[gesture] + '-' + time.strftime('%Y%m%d-%H%M%S')
+    extension = '.png'
+
+    if notify:
+        print('Saving {}'.format(folder + name + extension))
+
+    # Save image
+    cv2.imwrite(folder + name + extension, img)
+
+def quit(delay=0):
+    pg.time.wait(delay)
+    pg.quit()
+    sys.exit()
+
+try:
+
+    # Load classifier from pickle file
+    filename = 'clf.pkl'
+    f = open(filename, 'rb')
+    clf = pickle.load(f)
+    f.close()
+
+    # Create camera object with pre-defined settings
+    cam = rps.cameraSetup()
+
+    # Initialize last gesture value
+    lastGesture = 0
+
+    # Define score at which game ends
+    endScore = 5
+
+    # Initialize pygame
+    pg.init()
+    sWidth = 640
+    sHeight = 480
+    surf = pg.display.set_mode((sWidth, sHeight))
+    pg.display.set_caption(CAPTION)
+
+    # Initialize GUI
+    gui = rpsgui.RPSGUI(surf)
+
+    # Load static images for computer gestures
+    coImgs = {}
+    img = cv2.imread('img/rock/rock-20171009-233418.png', cv2.IMREAD_COLOR)
+    coImgs[rps.ROCK] = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = cv2.imread('img/paper/paper-20170719-165554.png', cv2.IMREAD_COLOR)
+    coImgs[rps.PAPER] = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = cv2.imread('img/scissors/scissors-20171009-234302.png', cv2.IMREAD_COLOR)
+    coImgs[rps.SCISSORS] = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    # Load green image
+    greenImg = cv2.imread('img/test/green.png', cv2.IMREAD_COLOR)
+    greenImg = cv2.cvtColor(greenImg, cv2.COLOR_BGR2RGB)
+
+    while True:
+
+        # Get image from camera
+        img = imp.crop(cam.getOpenCVImage())
+
+        # Convert image to RGB (from BGR)
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+        # Set player image to imgRGB
+        gui.setPlImg(imgRGB)
+
+        # Get grayscale image
+        gray = imp.getGray(imgRGB, threshold=.1)
+
+        # Count non-background pixels
+        nonZero = np.count_nonzero(gray)
+
+        # Define waiting time
+        waitTime = 0
+
+        # Parameters for saving new images
+        gesture = None
+        notify = False
+
+        # Check if player hand is present
+        if nonZero > 9000:
+
+            # Predict gesture
+            predGesture = clf.predict([gray])[0]
+
+            if predGesture == lastGesture:
+                successive += 1
+            else:
+                successive = 0
+
+            if successive == 2:
+                print('Player: {}'.format(rps.gestureTxt[predGesture]))
+                waitTime = 3000
+                gesture = predGesture
+
+                # Computer gesture
+                computerGesture = random.randint(1,3)
+                print('Computer: {}'.format(rps.gestureTxt[computerGesture]))
+
+                # Set computer image to computer gesture
+                gui.setCoImg(coImgs[computerGesture])
+
+                diff = computerGesture - predGesture
+                if diff in [-2, 1]:
+                    print('Computer wins!')
+                    gui.setWinner('computer')
+                elif diff in [-1, 2]:
+                    print('Player wins!')
+                    gui.setWinner('player')
+                else:
+                    print('Tie')
+                    gui.setWinner('tie')
+                print('Score: player {}, computer {}\n'.format(gui.plScore,
+                                                             gui.coScore))
+
+            lastGesture = predGesture
+
+        else:
+
+            lastGesture = 0
+
+            # Set computer image to green
+            gui.setCoImg(greenImg)
+            gui.setWinner()
+
+        # Draw GUI
+        gui.draw()
+
+        # Flip pygame display
+        pg.display.flip()
+
+        # Wait
+        pg.time.wait(waitTime)
+
+        if gesture:
+            # Save new image
+            saveImage(img, gesture, notify)
+
+        # Check if scores reach endScore (end of game)
+        if gui.plScore == endScore or gui.coScore == endScore:
+            if gui.coScore > gui.plScore:
+                print('Game over, computer wins...')
+            else:
+                print('Game over, player wins!!!')
+            quit()
+
+        # Check pygame events
+        for event in pg.event.get():
+            if event.type == pg.locals.QUIT:
+                quit()
+
+    quit()
+
+finally:
+    f.close()
+    cv2.destroyAllWindows()
+    cam.close()
---
+++ rpsgui.py
@@ -0,0 +1,82 @@
+import numpy as np
+
+import pygame as pg
+import pygame.freetype
+
+import cv2
+
+class RPSGUI():
+
+    def __init__(self, surf):
+        self.surf = surf
+        self.plScore = 0
+        self.coScore = 0
+        self.plImg = pg.Surface((200, 300))
+        self.coImg = pg.Surface((200, 300))
+        self.plImgPos = (380, 165)
+        self.coImgPos = (60, 165)
+        self.plZone = pg.Surface((250, 330))
+        self.coZone = pg.Surface((250, 330))
+        self.plZonePos = (355, 150)
+        self.coZonePos = (35, 150)
+        self.winner = None
+        self.plcoFont = pg.freetype.SysFont('', 30)
+        self.scoreFont = pg.freetype.SysFont('', 100)
+
+        # colors
+        self.WHITE = (255, 255, 255)
+        self.BLACK = (0, 0, 0)
+        self.RED = (255, 0, 0)
+        self.GREEN = (0, 255, 0)
+        self.BLUE = (0, 0, 255)
+
+    def draw(self):
+        # Fill surface with background color
+        self.surf.fill(self.WHITE)
+
+        # Render computer and player text
+        self.plcoFont.render_to(self.surf, (415, 10), 'PLAYER',
+                                 fgcolor=self.BLACK)
+        self.plcoFont.render_to(self.surf, (75, 10), 'COMPUTER',
+                                 fgcolor=self.BLACK)
+
+        # Set computer and player zone colors
+        if self.winner == 'player':
+            self.plZone.fill(self.GREEN)
+            self.coZone.fill(self.RED)
+        elif self.winner == 'computer':
+            self.plZone.fill(self.RED)
+            self.coZone.fill(self.GREEN)
+        elif self.winner == 'tie':
+            self.plZone.fill(self.BLUE)
+            self.coZone.fill(self.BLUE)
+        else:
+            self.plZone.fill(self.WHITE)
+            self.coZone.fill(self.WHITE)
+
+        # Blit computer and player zone
+        self.surf.blit(self.plZone, self.plZonePos)
+        self.surf.blit(self.coZone, self.coZonePos)
+
+        # Blit computer and player images
+        self.surf.blit(self.plImg, self.plImgPos)
+        self.surf.blit(self.coImg, self.coImgPos)
+
+        # Render computer and player scores
+        self.scoreFont.render_to(self.surf, (452,65), str(self.plScore),
+                                 fgcolor=self.BLACK)
+        self.scoreFont.render_to(self.surf, (132,65), str(self.coScore),
+                                 fgcolor=self.BLACK)
+
+    def setCoImg(self, img):
+        self.coImg = pg.surfarray.make_surface(img[:,::-1,:])
+
+    def setPlImg(self, img):
+        self.plImg = pg.surfarray.make_surface(img[::-1,:,:])
+
+    def setWinner(self, winner=None):
+        self.winner = winner
+        if winner == 'player':
+            self.plScore += 1
+        elif winner == 'computer':
+            self.coScore += 1
